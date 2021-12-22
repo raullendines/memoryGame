4e90fcd328279418657fcd906d42d05fbbe04762
Raul Lendines Ramos <91599628+raullendines@users.noreply.github.com>
2021-12-22 11:11:07 +0100

    Add files via upload

---
+++ jsGameRaul/mainGameRaul.js
@@ -0,0 +1,65 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    menu();
+});
+
+//Juego modo facil
+function juegoFacil() {
+    divCentrar = document.getElementById("centrar");
+
+    document.getElementById('memory-game').removeChild(divCentrar);
+
+    for (let i = 0; i < 12; i++) {
+        card();
+    }
+
+    mostrarEventos();
+
+    mezclar(iconosDaw);     //Cuando tenga dos sets habra un if
+
+    timedCount();
+}
+
+//Juego modo dificil
+function juegoDificil() {
+    divCentrar = document.getElementById("centrar");
+    document.getElementById('memory-game').removeChild(divCentrar);
+
+    for (let i = 0; i < 12; i++) {
+        cartasDificil();
+    }
+    
+    mostrarEventos();
+    mezclar(iconosDaw);     //Cuando tenga dos sets habra un if
+    timedCount();
+}
+
+//Fin del juego
+function gameOver() {
+    //Recorro 12 veces la funcion de borrar cartas
+    for (let i = 0; i < 12; i++) {
+        removeCards();
+    }
+
+    tiempoFinal = secondsInicial - seconds;
+
+    clearTimeout(time);
+    document.getElementById("timer").innerHTML = " ";
+
+    pantallaFinal();
+
+}
+
+//Borrar cartas al final del juego
+function removeCards() {
+    if (document.getElementById("card") != null) {
+        card = document.getElementById("card");
+
+        document.getElementById('memory-game').removeChild(card);
+        
+    }
+}
+
+
+
